5845b2847af7b99130901dd8480eecf22ad06b10
hyun <edocnuyh@gmail.com>
2024-09-24 01:34:25 +0900

    feat: 특정유저의 포인트 충전 기능 및 테스트

--- src/main/java/io/hhplus/tdd/ErrorResponse.java
+++ src/main/java/io/hhplus/tdd/ErrorResponse.java
@@ -4,4 +4,8 @@ public record ErrorResponse(
         String code,
         String message
 ) {
+
+    public static ErrorResponse error(ErrorCode code) {
+        return new ErrorResponse(code.getCode(), code.getMessage());
+    }
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -2,6 +2,8 @@ package io.hhplus.tdd.point;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.hhplus.tdd.CustomException;
+import io.hhplus.tdd.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,69 @@ class PointServiceTest {
 
         // 검증
         assertEquals(id, userPoint.id());
+    }
+
+    @Test
+    @DisplayName("특정유저의 포인트 충전 시 유저 id가 0 이하인 경우는 USER_ID_ERROR 오류가 발생한다.")
+    void chargeUserPoint_then_USER_ID_ERROR() {
+        // case1
+        // 특정유저의 id, 충전 금액
+        long id = 0;
+        long amount = 2000;
+
+        // 충전시 오류발생
+        CustomException e = assertThrows(CustomException.class, () -> {
+            pointService.charge(id, amount);
+        });
+
+        // 오류코드 검증
+        assertEquals(ErrorCode.USER_ID_ERROR.getCode(), e.getErrorCode().getCode());
+
+        // case2
+        long id2 = -1;
+        CustomException e2 = assertThrows(CustomException.class, () -> {
+            pointService.charge(id, amount);
+        });
+        assertEquals(ErrorCode.USER_ID_ERROR.getCode(), e2.getErrorCode().getCode());
+    }
+
+    @Test
+    @DisplayName("특정유저의 포인트 충전 시 충전금액이 0 이하인 경우는 POINT_AMOUNT_ERROR 오류가 발생한다.")
+    void chargeUserPoint_then_POINT_AMOUNT_ERROR() {
+        // case1
+        // 특정유저의 id, 충전 금액
+        long id = 1L;
+        long amount = 0;
+
+        // 충전시 오류발생
+        CustomException e = assertThrows(CustomException.class, () -> {
+            pointService.charge(id, amount);
+        });
+
+        // 오류코드 검증
+        assertEquals(ErrorCode.POINT_AMOUNT_ERROR.getCode(), e.getErrorCode().getCode());
+
+        // case2
+        long amount2 = -2000;
+        CustomException e2 = assertThrows(CustomException.class, () -> {
+            pointService.charge(id, amount2);
+        });
+        assertEquals(ErrorCode.POINT_AMOUNT_ERROR.getCode(), e2.getErrorCode().getCode());
+    }
+
+    @Test
+    @DisplayName("특정유저의 포인트 충전 성공케이스 테스트")
+    void chargeUserPoint() {
+        // case1
+        long id = 1L;
+        long amount = 2000;
+
+        UserPoint userPoint = pointService.charge(id, amount);
+        assertEquals(userPoint.point(), amount);
+
+        // case2 (같은유저가 한번더 충전)
+        UserPoint userPoint2 = pointService.charge(id, amount);
+        assertEquals(userPoint2.point(), 2 * amount);
     }
 
 }
