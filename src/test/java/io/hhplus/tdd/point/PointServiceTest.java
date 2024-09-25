4ec94f01afa186cfe321ed87a720cb9375e249ba
hyun <edocnuyh@gmail.com>
2024-09-26 02:06:43 +0900

    test: 포인트 사용 테스트코드 추가

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -2,12 +2,15 @@ package io.hhplus.tdd.point;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import io.hhplus.tdd.CustomException;
 import io.hhplus.tdd.ErrorCode;
 import io.hhplus.tdd.point.repository.PointHistoryRepository;
 import io.hhplus.tdd.point.repository.UserPointRepository;
+import java.util.concurrent.atomic.AtomicLong;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -102,18 +105,28 @@ class PointServiceTest {
     @Test
     @DisplayName("특정유저의 포인트 충전 성공케이스 테스트")
     void chargeUserPoint() {
-        // case1
         long id = 1L;
-        long amount = 2000L;
-        long existingPoint = 3000L;
-        when(userPointRepository.selectById(id))
-            .thenReturn(new UserPoint(id,existingPoint,System.currentTimeMillis()));
-        when(userPointRepository.insertOrUpdate(id,amount+existingPoint))
-            .thenReturn(new UserPoint(id,amount+existingPoint,System.currentTimeMillis()));
-        TransactionType type = TransactionType.CHARGE;
+        AtomicLong currentPoint = new AtomicLong(3000L); // 충전,사용 케이스 동시 테스트 위한 가변변수 선언.
 
-        UserPoint userPoint = pointService.chargeOrUse(id, amount, type);
+        when(userPointRepository.selectById(id))
+            .thenAnswer(invocation -> new UserPoint(id, currentPoint.get(), System.currentTimeMillis()));
+
+        when(userPointRepository.insertOrUpdate(eq(id),anyLong()))
+            .thenAnswer(invocation -> {
+                long updatedPoint = invocation.getArgument(1);
+                currentPoint.set(updatedPoint);
+                return new UserPoint(id,updatedPoint,System.currentTimeMillis());
+            });
+
+        // 충전케이스 테스트
+        long charge = 2000L;
+        UserPoint userPoint = pointService.chargeOrUse(id, charge, TransactionType.CHARGE);
         assertEquals(5000L, userPoint.point());
+
+        // 사용케이스 테스트
+        long use = 200L;
+        UserPoint userPoint2 = pointService.chargeOrUse(id, use, TransactionType.USE);
+        assertEquals(4800L,  userPoint2.point());
     }
 
 }
